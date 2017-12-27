4444b2aab5a329daa97697808b5c4534402ea1a7
Dmitry Kubikov <dmitry.kubikov@ibm.com>
2017-12-27 12:49:55 -0500

    initial commit

---
+++ finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
@@ -0,0 +1,50 @@
+// tag::comment[]
+/*******************************************************************************
+ * Copyright (c) 2017 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+// end::comment[]
+package io.openliberty.guides.hello.it;
+
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
+import org.apache.commons.httpclient.methods.GetMethod;
+
+public class EndpointIT {
+    private static String URL;
+
+    @BeforeClass
+    public static void init() {
+        // tag::URL[]
+        String port = System.getProperty("liberty.test.port");
+        String war = System.getProperty("war.name");
+        URL = "http://localhost:" + port + "/" + war + "/" + "servlet";
+        // end::URL[]
+    }
+
+    @Test
+    public void testServlet() throws Exception {
+        HttpClient httpClient = new HttpClient();
+        GetMethod httpGetMethod = new GetMethod(URL);
+        // tag::clicklink[]
+        try {
+            int actualStatusCode = httpClient.executeMethod(httpGetMethod);
+            int expectedStatusCode = HttpStatus.SC_OK;
+            assertEquals("HTTP GET failed", expectedStatusCode, actualStatusCode);
+            String response = httpGetMethod.getResponseBodyAsString(1000);
+            assertTrue("Unexpected response body", response.contains("Hello! Is Gradle working for you?"));
+        } finally {
+            httpGetMethod.releaseConnection();
+        }
+        // end::clicklink[]
+    }
+}

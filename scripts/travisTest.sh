4bc116ec84b0472ec194c1dc85a2d7c8399b473a
Gilbert Kwan <gkwan@ca.ibm.com>
2020-09-22 13:04:44 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -1,5 +1,6 @@
 ./gradlew -version
 ./gradlew tasks
-./gradlew build -x openBrowser -i
+./gradlew clean
+./gradlew build -b ./build.gradle -x openBrowser -i
 curl http://localhost:9080/GradleSample/servlet | grep Hello
 ./gradlew libertyStop

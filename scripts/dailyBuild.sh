5ca3ac0074e744927d7a9f21fed637ddfa26cd99
Austin Seto <austin.seto@ibm.com>
2020-11-25 16:13:47 -0500

    Prep for daily builds

---
+++ scripts/dailyBuild.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while getopts t:d:b:u: flag;
+do
+    case "${flag}" in
+        t) DATE="${OPTARG}";;
+        d) DRIVER="${OPTARG}";;
+        b) BUILD="${OPTARG}";;
+        u) DOCKER_USERNAME="${OPTARG}";;
+    esac
+done
+
+sed -i "\#end::buildScript[]#a liberty { install { runtimeUrl=\"https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"\"}}" build.gradle
+cat build.gradle
+
+../scripts/testApp.sh

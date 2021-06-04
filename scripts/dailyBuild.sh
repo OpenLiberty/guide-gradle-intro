bfc06b20815a8e3d77f0827ea82079ea8cf262cd
Gilbert Kwan <gkwan@ca.ibm.com>
2021-06-04 11:54:53 -0400

    Revert "Merge staging to prod - update versions (#103)"

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-while getopts t:d: flag;
+while getopts t:d:b:u: flag;
 do
     case "${flag}" in
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
-        *) echo "Invalid option";;
+        b) BUILD="${OPTARG}";;
+        u) DOCKER_USERNAME="${OPTARG}";;
     esac
 done
 
-sed -i "\#end::buildscript#a liberty { install { runtimeUrl=\"https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER\"}}" build.gradle
+sed -i "\#end::buildscript#a liberty { install { runtimeUrl=\"https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"\"}}" build.gradle
 cat build.gradle
 
 ../scripts/testApp.sh

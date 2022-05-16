0071a31540ce85b72c61adc7deda05bf04f1fe62
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-16 17:42:58 -0400

    update versions

--- finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
+++ finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     IBM Corporation - Initial implementation
  *******************************************************************************/
 // end::copyright[]
 package io.openliberty.guides.hello.it;
@@ -33,7 +33,7 @@ public class EndpointIT {
         System.out.println("URL: " + URL);
     }
     // end::init[]
-    
+
     // tag::test[]
     @Test
     // end::test[]

3d289add89705c87247f51e09de80b71a4dd643b
Gilbert Kwan <gkwan@ca.ibm.com>
2022-05-19 13:16:32 -0400

    Version update mp5 (#115)
    * update versions
    
    * version mp5 update
    
    * version mp5 update
    
    * version mp5 update
    
    * linter fix
    
    * Delete EndpointIT.class
    
    Co-authored-by: t27gupta <tushar.gupta@uwaterloo.ca>

--- finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
+++ finish/src/test/java/io/openliberty/guides/hello/it/EndpointIT.java
@@ -1,18 +1,20 @@
 // tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017, 2020 IBM Corporation and others.
+ * Copyright (c) 2017, 2022 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - initial API and implementation
+ *     IBM Corporation - Initial implementation
  *******************************************************************************/
 // end::copyright[]
 package io.openliberty.guides.hello.it;
 // tag::import[]
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -20,26 +22,27 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 // end::import[]
+
 // tag::endpointit[]
 public class EndpointIT {
-    private static String URL;
+    private static String webURL;
 
     @BeforeAll
     // tag::init[]
     public static void init() {
         String port = System.getProperty("http.port");
         String context = System.getProperty("context.root");
-        URL = "http://localhost:" + port + "/" + context + "/" + "servlet";
-        System.out.println("URL: " + URL);
+        webURL = "http://localhost:" + port + "/" + context + "/" + "servlet";
+        System.out.println("URL: " + webURL);
     }
     // end::init[]
-    
+
     // tag::test[]
     @Test
     // end::test[]
     public void testServlet() throws Exception {
         HttpClient httpClient = new HttpClient();
-        GetMethod httpGetMethod = new GetMethod(URL);
+        GetMethod httpGetMethod = new GetMethod(webURL);
         // tag::try[]
         try {
             int actualStatusCode = httpClient.executeMethod(httpGetMethod);
